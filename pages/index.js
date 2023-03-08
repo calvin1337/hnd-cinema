4c61436f4c57178e2859a75b8366cb7a06a0eab3
Calvin Donaldson <calvindonaldson1997313@gmail.com>
2023-03-08 05:07:42 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>HNCD Cinema</title>
+        <meta name="description" content="Cinema Booking App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        
+      </main>
+    </>
+  )
+}
