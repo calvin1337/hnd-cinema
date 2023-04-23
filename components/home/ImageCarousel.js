1da1f74cc7b8c0590ca581b6a7a3323207fab703
Calvin D <57333694+calvin1337@users.noreply.github.com>
2023-04-23 18:30:17 +0000

    homepage

--- components/home/ImageCarousel.js
+++ components/home/ImageCarousel.js
@@ -10,30 +10,59 @@ import "swiper/css/pagination";
 
 // Image carousel using swiper js
 
+
+// ADD STYLES NEED OVERLAY AND EDIT IMAGES.
+
 const ImageCarousel = () => {
   return (
-    <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper h-screen border-green-200-400 border">
-        {/* Slide 1 */}
-        <SwiperSlide>
-          <div className="h-full flex justify-center items-center">
-            <img src=" https://via.placeholder.com/1920x1735" alt="" />
-          </div>
-          </SwiperSlide>
-          {/* Slide 2 */}
-        <SwiperSlide>
-          <div className="h-full flex justify-center items-center">
-          <img src=" https://via.placeholder.com/1920x1735" alt="" />
-          </div></SwiperSlide>
-          {/* Slide 3 */}
-        <SwiperSlide>
-          <div className="h-full flex justify-center items-center">
-          <img src=" https://via.placeholder.com/1920x1735" alt="" />
-            </div>
-        </SwiperSlide>
-      </Swiper>
-    </>
-  )
-}
+    <Swiper
+      navigation={true}
+      modules={[Navigation]}
+      className="mySwiper w-screen"
+      style={{ height: 'calc(100vh - 80px)'}}
+    >
+      {/* Slide 1 */}
+      <SwiperSlide
+        className="w-screen h-full"
+        style={{ width: '100%', height: '100%' }}
+      >
+        <div className="h-full flex justify-center items-center">
+          <img
+            src="https://images.pexels.com/photos/7234238/pexels-photo-7234238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </SwiperSlide>
 
+      {/* Slide 2 */}
+      <SwiperSlide
+        className="w-screen h-full"
+        style={{ width: '100%', height: '100%' }}
+      >
+        <div className="h-full flex justify-center items-center">
+          <img
+            src="https://via.placeholder.com/1920x1735"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </SwiperSlide>
+
+      {/* Slide 3 */}
+      <SwiperSlide
+        className="w-screen h-full"
+        style={{ width: '100%', height: '100%' }}
+      >
+        <div className="h-full flex justify-center items-center">
+          <img
+            src="https://via.placeholder.com/1920x1735"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
 export default ImageCarousel
