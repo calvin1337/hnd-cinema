bcfd6fbd0db19150ef1cd632e1af821f7065c28a
Calvin Donaldson <calvindonaldson1997313@gmail.com>
2023-05-07 01:58:05 +0100

    booking

--- components/booking/Payment.js
+++ components/booking/Payment.js
@@ -1,16 +1,26 @@
 import { useEffect } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
 
 export const Payment = (props) => {
   useEffect(() => {
-    const fetchShowing = async () => {
+    const addBookingToDatabase = async () => {
       try {
         const showingRef = doc(db, 'showings', props.booked.showingID);
         const docSnap = await getDoc(showingRef);
         if (docSnap.exists()) {
           const showingData = docSnap.data();
           console.log('Showing Data:', showingData);
+
+          // Get the existing bookings array
+          const existingBookings = showingData.bookings || [];
+
+          // Add the new booking to the existing bookings array
+          const updatedBookings = [...existingBookings, ...props.seats];
+
+          // Update the bookings field in the document
+          await updateDoc(showingRef, { bookings: updatedBookings });
+          console.log('Booking added to the database.');
         } else {
           console.log('No such document!');
         }
@@ -19,9 +29,11 @@ export const Payment = (props) => {
       }
     };
 
-    fetchShowing();
-  }, [props.booked.showingID]);
-console.log(props.booked)
+    addBookingToDatabase();
+  }, [props.booked.showingID, props.seats]);
+
+  console.log(props.booked);
+
   return (
     <div>
       {/* Payment component content */}
