17f8be017680cc5573afd9699aa79f890b91c753
Calvin Donaldson <calvindonaldson1997313@gmail.com>
2023-05-07 22:51:30 +0100

    testing

--- pages/Admin.js
+++ pages/Admin.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import addMovie from '../components/admin/addMovie'
+// import React from 'react'
+// import addMovie from '../components/admin/addMovie'
 
-const Admin = () => {
-  return (
-    <div>
-      <h1>test</h1>
-      <addMovie />
-    </div>
-  )
-}
+// const Admin = () => {
+//   return (
+//     <div>
+//       <h1>test</h1>
+//       <addMovie />
+//     </div>
+//   )
+// }
 
-export default Admin
+// export default Admin
