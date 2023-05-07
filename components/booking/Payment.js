329f6a6d4d66279a2217bd55bfe8d66939b00c9d
Calvin Donaldson <calvindonaldson1997313@gmail.com>
2023-05-07 21:20:35 +0100

    booking

--- components/booking/Payment.js
+++ components/booking/Payment.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
+import React from 'react';
+import { AuthContext } from '@/hooks/AuthContext';
 
 export const Payment = (props) => {
   const [bookingSuccess, setBookingSuccess] = useState(false);
+  const { user } = React.useContext(AuthContext);
 
   useEffect(() => {
     const addBookingToDatabase = async () => {
@@ -26,6 +29,23 @@ export const Payment = (props) => {
 
           // Set booking success to true
           setBookingSuccess(true);
+
+          // Update the user's account with the booking details
+          const userRef = doc(db, 'users', user.uid); // Replace 'users' with the appropriate collection name for users
+          const userDocSnap = await getDoc(userRef);
+          if (userDocSnap.exists()) {
+            const userData = userDocSnap.data();
+
+            // Add the new booking to the user's bookings array
+            const userBookings = userData.bookings || [];
+            const updatedUserBookings = [...userBookings, { seats: props.seats, showingID: props.booked.showingID, title: props.booked.title, time: props.booked.time, screen: 4}];
+
+            // Update the bookings field in the user document
+            await updateDoc(userRef, { bookings: updatedUserBookings });
+            console.log('Booking added to the user account.');
+          } else {
+            console.log('User document not found!');
+          }
         } else {
           console.log('No such document!');
         }
@@ -35,7 +55,7 @@ export const Payment = (props) => {
     };
 
     addBookingToDatabase();
-  }, [props.booked.showingID, props.seats]);
+  }, [props.booked.showingID, props.seats, user.uid]);
 
   console.log(props.booked);
 
--- components/user/ViewBookings.js
+++ components/user/ViewBookings.js
@@ -1,24 +1,59 @@
-
+import React, { useContext, useEffect, useState } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { AuthContext } from '@/hooks/AuthContext';
 
 const ViewBookings = () => {
-  return (
-  <div className="container mx-auto py-4 h-96">
-  <h2 className="text-2xl font-bold mb-4">My Bookings</h2>
-  
-    <div className="px-4 py-3 border-b border-gray-200">
-      
-      <div className="flex justify-between">
-        <div className="w-1/4">Booking ID</div>
-        <div className="w-1/4">Date</div>
-        <div className="w-1/4">Time</div>
-        <div className="w-1/4">Movie</div>
-        <div className="w-1/4">Screen</div>
-        <div className="w-1/4">Seats</div>
-      </div>
-    </div>
-    
-</div>
-  )
-}
+  const [bookings, setBookings] = useState([]);
+  const { user } = useContext(AuthContext);
 
-export default ViewBookings
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const userRef = doc(db, 'users', user.uid);
+        const userDoc = await getDoc(userRef);
+
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          const userBookings = userData.bookings || [];
+          setBookings(userBookings);
+        }
+      } catch (error) {
+        console.error('Error fetching bookings:', error);
+      }
+    };
+
+    if (user) {
+      fetchBookings();
+    }
+  }, [user]);
+
+  return (
+    <div className="container mx-auto py-4 h-96">
+      <h2 className="text-2xl font-bold mb-4">My Bookings</h2>
+      <div className="px-4 py-3 border-b border-gray-200">
+        <div className="flex justify-between">
+          <div className="w-1/4">Booking ID</div>
+          <div className="w-1/4">Seats</div>
+          <div className="w-1/4">Movie</div>
+          <div className="w-1/4">Time</div>
+          <div className="w-1/4">Screen</div>
+        </div>
+      </div>
+      {/* Display the bookings */}
+      {bookings.map((booking) => (
+        <div key={booking.bookingID} className="px-4 py-3 border-b border-gray-200">
+          <div className="flex justify-between">
+            <div className="w-1/4">{booking.showingID}</div>
+            <div className="w-1/4">{booking.seats.join(', ')}</div>
+            <div className="w-1/4">{booking.title}</div>
+            <div className="w-1/4">{booking.time}</div>
+            <div className="w-1/4">{booking.screen}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ViewBookings;
